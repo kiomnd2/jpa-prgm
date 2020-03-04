0287507470ba03429475b6e3c9defc5d732e716c
kiomnd2 <kiomnd2@gmail.com>
2020-03-04 23:08:50 +0900

    JPA 영속성 컨텍스트

--- ex1-hello-jpa/src/main/java/hellpjpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellpjpa/JpaMain.java
@@ -41,6 +41,19 @@ public class JpaMain {
                 System.out.println(member.getName());
             }
 
+            /*//커밋 이후 모두 실행됨..
+            Member member1 = new Member(150L, "A");
+            Member member2 = new Member(160L, "A");
+*/
+
+            Member member = em.find(Member.class , 150L);
+            member.setName("AAAAA"); //영속 상태.. 변경내용 추적
+
+            em.detach(member); // 준영속 상태. JPA에서 해당 엔티티를 관리하지 않음
+            //쿼리 실행시 , 조회쿼리는 실행되도 update 쿼리는 실행하지 않음(setName)
+//            em.clear(); 영속성 컨텍스트를 초기화 함
+//            em.close(); //영속성 컨텍스트를 종료..
+
             tx.commit();
         } catch(Exception e)
         {
--- ex1-hello-jpa/src/main/java/hellpjpa/Member.java
+++ ex1-hello-jpa/src/main/java/hellpjpa/Member.java
@@ -10,6 +10,16 @@ public class Member {
     private Long id;
     private String name;
 
+
+    //엔티티 정의시 기본 생성자가 필요함
+    public Member() {
+    }
+
+    public Member(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
     public Long getId() {
         return id;
     }
